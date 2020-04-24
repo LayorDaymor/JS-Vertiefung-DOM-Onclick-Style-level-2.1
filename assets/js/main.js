b6b1f10945991d026ec9c28d082bc1cb6bea1b39
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-24 20:15:28 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+const wPlace =document.getElementById("wPlace");
+const navHome=document.getElementById("navHome");
+const navNews=document.getElementById("navNews");
+const navContact=document.getElementById("navContact");
+
+function changeMe(){
+    navHome.classList.toggle("reversingText")
+    navHome.style.backgroundColor="palevioletred"
+    
+     
+
+}
